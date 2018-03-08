2a822fdebb711712b347837594199ed733ec3876
Cristian Ruz <i42runac@uco.es>
2018-03-08 13:24:54 +0100

    Cambios día 3 : implementados crear y editar tarea

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Task;
 use App\Form\TaskType;
 use App\Repository\TaskRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -27,13 +28,39 @@ class TaskController extends Controller
     /**
      * @Route("/task/new" , name="task_new")
      */
-    public function new()
+    public function new(Request $request)
     {
         $task = new Task();
-       $form = $this->createForm(TaskType::class , $task );
-
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $om = $this->getDoctrine()->getManager();
+            $om->persist($task);
+            $om->flush();
+            $this->addFlash('positive', 'Tarea creada');
+            return $this->redirectToRoute('homepage');
+        }
         return $this->render('task/new.html.twig', [
-            'form' => $form->createView() ,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/task/{id}/edit", name="task_edit")
+     */
+    public function edit(Request $request, Task $task)
+    {
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $om = $this->getDoctrine()->getManager();
+            $om->persist($task);
+            $om->flush();
+            $this->addFlash('positive', 'Tarea editada');
+            return $this->redirectToRoute('homepage');
+        }
+        return $this->render('task/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
